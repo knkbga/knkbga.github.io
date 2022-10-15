72352f40901f4ee38bb57325ccf7292fc071e246
Managevent <managevent@Kanaks-MacBook-Pro.local>
2022-10-15 12:52:17 +0530

    chore: initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express')
+var app = express()
+var path = require('path')
+
+app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+   res.sendFile(path.join(__dirname + '/public/index.html'))
+})
+app.get('/resume', function (req, res) {
+   res.sendFile(path.join(__dirname + '/public/resume.pdf'))
+})
+
+app.listen(8080, () => {
+   console.log('Listening on port 8080')
+})
