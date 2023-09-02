99f65b69beb52d0cb832858e33791e93e5fbf3c7
Kanak Bagga <kanakbagga1@gmail.com>
2023-09-02 15:51:39 +0530

    feat: scroll behaviours fixed for safari browser

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -8,3 +8,27 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
   hamburger.classList.remove("active")
   nav_menu.classList.remove("active")
 }))
+
+/** Scrolls the element into view
+ * Manually created since Safari does not support the native one inside an iframe
+*/
+function scrollElementIntoView(element, behavior) {
+
+  let scrollTop = window.pageYOffset || element.scrollTop
+
+  // Furthermore, if you have for example a header outside the iframe 
+  // you need to factor in its dimensions when calculating the position to scroll to
+  const headerOutsideIframe = window.parent.document.getElementsByClassName('myHeader')[0].clientHeight
+
+  const finalOffset = element.getBoundingClientRect().top + scrollTop + headerOutsideIframe
+
+  window.parent.scrollTo({
+    top: finalOffset,
+    behavior: behavior || 'auto'
+  })
+}
+
+function scrollToSectionID(sectionID) {
+  const element = document.getElementById(`${sectionID}`)
+  scrollElementIntoView(element, 'smooth')
+}
