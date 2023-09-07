c17dcac7bbf93f09832d929ebd7fa1742cf56d57
Kanak Bagga <kanakbagga1@gmail.com>
2023-09-07 23:43:30 +0530

    fix: slow loading of carousel arrows test

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -1,34 +1,36 @@
-const hamburger = document.querySelector(".hamburger")
-const nav_menu = document.querySelector(".nav-menu")
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active")
-  nav_menu.classList.toggle("active")
-})
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-  hamburger.classList.remove("active")
-  nav_menu.classList.remove("active")
-}))
-
-/** Scrolls the element into view
- * Manually created since Safari does not support the native one inside an iframe
-*/
-function scrollElementIntoView(element, behavior) {
-
-  let scrollTop = window.pageYOffset || element.scrollTop
-
-  // Furthermore, if you have for example a header outside the iframe 
-  // you need to factor in its dimensions when calculating the position to scroll to
-  const headerOutsideIframe = window.parent.document.getElementsByClassName('myHeader')[0].clientHeight
-
-  const finalOffset = element.getBoundingClientRect().top + scrollTop + headerOutsideIframe
-
-  window.parent.scrollTo({
-    top: finalOffset,
-    behavior: behavior || 'auto'
+$(document).ready(function () {
+  const hamburger = document.querySelector(".hamburger")
+  const nav_menu = document.querySelector(".nav-menu")
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active")
+    nav_menu.classList.toggle("active")
   })
-}
+  document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active")
+    nav_menu.classList.remove("active")
+  }))
 
-function scrollToSectionID(sectionID) {
-  const element = document.getElementById(`${sectionID}`)
-  scrollElementIntoView(element, 'smooth')
-}
+  /** Scrolls the element into view
+   * Manually created since Safari does not support the native one inside an iframe
+  */
+  function scrollElementIntoView(element, behavior) {
+
+    let scrollTop = window.pageYOffset || element.scrollTop
+
+    // Furthermore, if you have for example a header outside the iframe 
+    // you need to factor in its dimensions when calculating the position to scroll to
+    const headerOutsideIframe = window.parent.document.getElementsByClassName('myHeader')[0].clientHeight
+
+    const finalOffset = element.getBoundingClientRect().top + scrollTop + headerOutsideIframe
+
+    window.parent.scrollTo({
+      top: finalOffset,
+      behavior: behavior || 'auto'
+    })
+  }
+
+  function scrollToSectionID(sectionID) {
+    const element = document.getElementById(`${sectionID}`)
+    scrollElementIntoView(element, 'smooth')
+  }
+})
--- public/scripts/project-section.js
+++ public/scripts/project-section.js
@@ -1,33 +1,35 @@
-var totalLength = 10
-$('.slick', '.vertical-slider').slick({
-  vertical: true,
-  verticalSwiping: true,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  prevArrow: false,
-  nextArrow: false,
-  dots: true,
-  infinite: true,
-  speed: 500,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  pauseOnHover: false,
-  draggable: false,
-  prevArrow: "<img class='a-left control-c prev slick-prev' width='10px' src='./public/images/left-carousel-arrow.svg'>",
-  nextArrow: "<img class='a-right control-c next slick-next' width='10px' src='./public/images/right-carousel-arrow.svg'>"
-})
+$(document).ready(function () {
+  var totalLength = 5
+  $('.slick', '.vertical-slider').slick({
+    vertical: true,
+    verticalSwiping: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: false,
+    nextArrow: false,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: false,
+    draggable: false,
+    prevArrow: "<img class='a-left control-c prev slick-prev' width='10px' >",
+    nextArrow: "<img class='a-right control-c next slick-next' width='10px' >"
+  })
 
-$('.slider').on('beforeChange', function (event, slick, currentSlide) { 
-  for (let i = 0; i < totalLength; i++) {
-    $(`.timeline li:nth-child(${i})`).removeClass('active')
-  }
-})
-$('.slider').on('afterChange', function (event, slick, currentSlide) {
-  $(`.timeline li:nth-child(${currentSlide + 1})`).addClass('active')
-})
+  $('.slider').on('beforeChange', function (event, slick, currentSlide) {
+    for (let i = 0; i < totalLength; i++) {
+      $(`.timeline li:nth-child(${i + 1})`).removeClass('active')
+    }
+  })
+  $('.slider').on('afterChange', function (event, slick, currentSlide) {
+    $(`.timeline li:nth-child(${currentSlide + 1})`).addClass('active')
+  })
 
-$('.timeline li[data-slide]').click(function (e) {
-  e.preventDefault()
-  var slideNumber = $(this).data('slide')
-  $('.slider').slick('slickGoTo', slideNumber - 1)
+  $('.timeline li[data-slide]').click(function (e) {
+    e.preventDefault()
+    var slideNumber = $(this).data('slide')
+    $('.slider').slick('slickGoTo', slideNumber - 1)
+  })
 })
